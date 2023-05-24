4f92659d63cb44f6aa04f4ab25d31fce1d868e10
Somesh Dattatraya Dahotre <131247687+totally-somesh@users.noreply.github.com>
2023-05-24 19:19:25 +0530

    While Loop In JS
    ~ Here, Basic while loop is used for iterating the desired code for 10 times.

---
+++ While Loop/while.js
@@ -0,0 +1,8 @@
+document.write("<h1> While Loop In JavaScript <h1>");
+document.write("<h3> Printing India ! For 10 Times </h3>");
+let x = 1;
+
+while(x <= 10){
+    document.write(x + " : <h1>India ! </h1><br>");
+    x = x+1;
+}
